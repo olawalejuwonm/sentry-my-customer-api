1ff365d8053fbb2bf3c3c3e144d2921fe60786c2
Lumen-X <onohsomtochukwubasil@gmail.com>
2020-07-22 01:11:21 +0100

    v2: store admin dashboard working

--- controllers/dashboardController.js
+++ controllers/dashboardController.js
@@ -1,29 +1,15 @@
 const userModel = require("../models/user");
 const storeAdminModel = require("../models/store_admin");
 const storeAssistantModel = require("../models/storeAssistant");
-const storeModel = require("../models/store");
+const Stores = require("../models/store");
 const customerModel = require("../models/customer");
+const transactionModel = require("../models/transaction");
 
 exports.storeAdminDashboard = async (req, res, next) => {
   const identifier = req.user.phone_number;
-  const role = req.user.user_role;
-
-  if (role != "store_admin") {
-    if (role == "store_assistant") {
-      var u = 1;
-    } else {
-      return next();
-    }
-  }
 
   const storeAdmin = await storeAdminModel.findOne({
-    $or: [
-      { identifier: req.user.phone_number, "local.user_role": req.user.user_role },
-      {
-        "assistants.phone_number": req.user.phone_number,
-        "assistants.user_role": req.user.user_role
-      }
-    ]
+    identifier: req.user.phone_number
   });
   if (!storeAdmin) {
     return res.status(404).json({
@@ -38,8 +24,12 @@ exports.storeAdminDashboard = async (req, res, next) => {
 
   try {
     const data = {};
-    const stores = storeAdmin.stores;
-    const assistants = storeAdmin.assistants;
+    const stores = await Stores.find({ store_admin_ref: storeAdmin._id });
+    console.log(stores.length);
+    const assistants = await storeAssistantModel.find({
+      store_admin_ref: storeAdmin._id
+    });
+
     //get number of stores
     data.storeCount = stores.length;
     //get number of assisstants
@@ -51,11 +41,11 @@ exports.storeAdminDashboard = async (req, res, next) => {
     data.recentTransactions = [];
     data.recentDebts = [];
 
-    stores.forEach(store => {
+    stores.forEach(async store => {
       //increment customer count by number of customers in each store
-      data.customerCount = data.customerCount + store.customers.length;
+      const customers = await customerModel.find({ store_ref_id: store._id });
+      data.customerCount = data.customerCount + customers.length;
 
-      const customers = store.customers;
       let date = new Date();
       //filter customers array to get all new customers
       const newCustomers = customers.filter(element => {
@@ -72,17 +62,19 @@ exports.storeAdminDashboard = async (req, res, next) => {
         );
       }
 
-      customers.forEach(customer => {
+      customers.forEach(async customer => {
+        const transactions = await transactionModel.find({
+          customer_ref_id: customer._id
+        });
         //push in transaction details for each customer
-        if (customer.transactions.length != 0) {
+        if (transactions.length != 0) {
           let obj = {};
           obj.storeName = store.store_name;
           obj.customerName = customer.name;
           //sort transactions by date
-          obj.transactions = customer.transactions.sort(compareTransactions);
+          obj.transactions = transactions.sort(compareTransactions);
           data.transactions.push(obj);
 
-          const transactions = customer.transactions;
           transactions.forEach(transaction => {
             //push in details of each transaction
             let obj = {};
@@ -229,11 +221,11 @@ exports.superAdminDashboard = async (req, res) => {
 
 exports.storeAssistantDashboard = async (req, res) => {
   const phone_number = req.user.phone_number;
-  const data ={};
-  
+  const data = {};
+
   const storeAdmin = await storeAdminModel.findOne({
     "assistants.phone_number": phone_number
-  })
+  });
   if (!storeAdmin) {
     return res.status(404).json({
       success: false,
@@ -245,28 +237,32 @@ exports.storeAssistantDashboard = async (req, res) => {
     });
   }
   try {
-    const assistant = storeAdmin.assistants.find(assistant => assistant.phone_number == phone_number);
+    const assistant = storeAdmin.assistants.find(
+      assistant => assistant.phone_number == phone_number
+    );
     data.name = assistant.name;
     data.email = assistant.email;
     data.phone_number = assistant.phone_number;
-    
+
     const store_id = assistant.store_id;
     if (!store_id) {
       return res.status(404).json({
         success: false,
-        message: 'Assistant does not belong to a store',
-        error:{
-          statusCode: '',
-          message: 'Assistant does not belong to a store'
+        message: "Assistant does not belong to a store",
+        error: {
+          statusCode: "",
+          message: "Assistant does not belong to a store"
         }
-      })
+      });
     }
-    const assistantStore = storeAdmin.stores.find(store => store._id == store_id);
+    const assistantStore = storeAdmin.stores.find(
+      store => store._id == store_id
+    );
     data.storeName = assistantStore.store_name;
-    data.storeAddress = assistantStore.shop_address
-    data.customerCount = 0; 
+    data.storeAddress = assistantStore.shop_address;
+    data.customerCount = 0;
     data.transactionCount = 0;
-    data.recentTransactions =[]
+    data.recentTransactions = [];
     data.debtCount = 0;
     data.debtAmount = 0;
     data.revenueCount = 0;
@@ -285,39 +281,46 @@ exports.storeAssistantDashboard = async (req, res) => {
           obj.transaction = transaction;
           data.recentTransactions.push(obj);
 
-          if (transaction.type.toLowerCase() == 'debt') {
+          if (transaction.type.toLowerCase() == "debt") {
             data.debtCount += 1;
-            try { data.debtAmount += parseFloat(transaction.amount); 
+            try {
+              data.debtAmount += parseFloat(transaction.amount);
             } catch (error) {
-              data.debtAmount += 0
+              data.debtAmount += 0;
             }
           }
-          if (transaction.type.toLowerCase() == 'debt' &&  transaction.status == true) {
+          if (
+            transaction.type.toLowerCase() == "debt" &&
+            transaction.status == true
+          ) {
             data.revenueCount += 1;
-            try { data.revenueAmount += parseFloat(transaction.amount); 
+            try {
+              data.revenueAmount += parseFloat(transaction.amount);
             } catch (error) {
-              data.revenueAmount += 0
-            } 
-          }
-          if (transaction.type.toLowerCase() == 'paid') {
-            data.revenueCount += 1;
-            try { data.revenueAmount += parseFloat(transaction.amount); 
-            } catch (error) {
-              data.revenueAmount += 0
+              data.revenueAmount += 0;
             }
           }
-          if (transaction.type.toLowerCase() == 'receivables') {
+          if (transaction.type.toLowerCase() == "paid") {
+            data.revenueCount += 1;
+            try {
+              data.revenueAmount += parseFloat(transaction.amount);
+            } catch (error) {
+              data.revenueAmount += 0;
+            }
+          }
+          if (transaction.type.toLowerCase() == "receivables") {
             data.receivablesCount += 1;
-            try { data.receivablesAmount +=  parseFloat(transaction.amount); 
+            try {
+              data.receivablesAmount += parseFloat(transaction.amount);
             } catch (error) {
-              data.receivablesAmount += 0
+              data.receivablesAmount += 0;
             }
           }
         }
-      })
-    })
+      });
+    });
     //sort transactions by time
-    data.recentTransactions.sort(compareRecentTransactions)
+    data.recentTransactions.sort(compareRecentTransactions);
 
     return res.status(200).json({
       success: true,
@@ -328,13 +331,12 @@ exports.storeAssistantDashboard = async (req, res) => {
     res.status(500).send({
       success: false,
       message: error.message,
-      error:{
+      error: {
         statusCode: 500,
         message: error.message
       }
-    })
+    });
   }
-  
 };
 
 exports.customerDashboard = async (req, res) => {
